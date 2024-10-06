ca8a348047f4f0298d87d246b3f25ccb8ae068b4
Nguyen Huynh <nhuy0017@student.monash.edu>
2024-10-06 20:51:23 +1100

    fourth

---
+++ piechartfemales.js
@@ -0,0 +1,5 @@
+var vg_2 = "piechartfemales.vg.json";
+
+vegaEmbed("#piechartfemales", vg_2).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
