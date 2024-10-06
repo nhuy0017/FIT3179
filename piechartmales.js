83fdea1c12fa36ee5e358d20cc27bc532fe29444
Nguyen Huynh <nhuy0017@student.monash.edu>
2024-10-06 21:54:21 +1100

    seventh

---
+++ piechartmales.js
@@ -0,0 +1,5 @@
+var vg_3 = "piechartmales.vg.json";
+
+vegaEmbed("#piechartmales", vg_3).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
